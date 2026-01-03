1ba06be90c3a4cb6f01fc5d0894d8d10451f3db5
Fletcher868 <isoinst@proton.me>
2026-01-03 05:47:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# 1. Ensure the binary is executable (Git often removes this)
+chmod +x ./pocketbase
+
+# 2. Run PocketBase using the $PORT environment variable
 ./pocketbase serve --http 0.0.0.0:"$PORT"
